422ca085e7279d886d7ef7aabcd4bf52ff19cb3b
Zhi Yang <zhiyang31@gmail.com>
2022-09-12 13:29:03 +0800

    Add navigation

--- app/src/main/java/com/example/readtrack/MainActivity.kt
+++ app/src/main/java/com/example/readtrack/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.readtrack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Grab the container fragment as NavHostFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.containerFragment) as NavHostFragment
+        // Initialize navController in this activity as navController in the fragment
+        this.navController = navHostFragment.navController
+
+        // Tell the bottom navigation bar to use the nav controller
+        findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(this.navController)
+
     }
 }
