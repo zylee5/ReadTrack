1eee70c695ec13102e680287d4b82413c84ed262
Zhi Yang <zhiyang31@gmail.com>
2022-09-29 13:25:42 +0800

    Add standalone date picker to each date text input

--- app/src/main/java/com/example/readtrack/fragments/AddBookFragment.kt
+++ app/src/main/java/com/example/readtrack/fragments/AddBookFragment.kt
@@ -1,7 +1,8 @@
 package com.example.readtrack.fragments
 
+import android.content.Context
 import android.os.Bundle
-import android.util.Log
+import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +12,8 @@ import androidx.fragment.app.activityViewModels
 import com.example.readtrack.databinding.FragmentAddBookBinding
 import com.example.readtrack.viewmodels.AddBookViewModel
 import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
+import com.google.android.material.textfield.TextInputEditText
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -25,38 +28,57 @@ class AddBookFragment : Fragment() {
         val binding = FragmentAddBookBinding.inflate(inflater, container, false)
         binding.vm = addBookViewModel
 
-        binding.pickDateButton.setOnClickListener {
-            // Initialize date range picker from material design library
-            val builder = MaterialDatePicker.Builder.dateRangePicker()
-            val picker = builder.build()
+        binding.startingDateTextField.setOnClickListener {
+            createDatePicker("Starting Date", binding.startingDateTextField)
+        }
 
-            // Inside this fragment host another child fragment
-            picker.show(childFragmentManager, picker.toString())
-
-            // Set the action for when save button is clicked
-            picker.addOnPositiveButtonClickListener {
-                // it = selected date range in Pair<Long!, Long!>
-                // it.first = start date
-                // it.second = finishing date
-                val dateFormatter = SimpleDateFormat("dd/MM/yyyy")
-                val startDate = dateFormatter.format(Date(it.first))
-                val finishingDate = dateFormatter.format(Date(it.second))
-                Toast.makeText(requireContext(), "Start date: ${startDate} \n Finishing date: ${finishingDate}", Toast.LENGTH_LONG).show()
-            }
-
-            // Set the action for when the cancel button is clicked
-            picker.addOnNegativeButtonClickListener {
-                Toast.makeText(requireContext(), "You have not selected a date range", Toast.LENGTH_LONG).show()
-            }
-
-            // Set the action for when the picker is canceled (back button, etc.)
-            picker.addOnCancelListener {
-                Toast.makeText(requireContext(), "You have not selected a date range", Toast.LENGTH_LONG).show()
-            }
-
+        binding.finishingDateTextField.setOnClickListener {
+            createDatePicker("Finishing Date", binding.finishingDateTextField)
         }
 
         return binding.root
     }
 
+    private fun createDatePicker(title: String, textField: TextInputEditText) {
+        // Initialize date range picker from material design library
+        val builder = MaterialDatePicker.Builder.datePicker()
+            .setTitleText(title)
+        val picker = builder.build()
+
+        // Inside this fragment host another child fragment
+        picker.show(childFragmentManager, picker.toString())
+
+        // Set the action for when save button is clicked
+        picker.addOnPositiveButtonClickListener {
+            // it = selected date in Long!
+            val dateFormatter = SimpleDateFormat("dd/MM/yyyy")
+            val formattedDate = dateFormatter.format(Date(it))
+//                val finishingDate = dateFormatter.format(Date(it.second))
+            textField.setText("$formattedDate")
+        }
+
+        // Set the action for when the cancel button is clicked
+        picker.addOnNegativeButtonClickListener {
+            Toast.makeText(requireContext(), "You have not selected a date", Toast.LENGTH_SHORT).show()
+        }
+
+        // Set the action for when the picker is canceled (back button, etc.)
+        picker.addOnCancelListener {
+            Toast.makeText(requireContext(), "You have not selected a date", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
+
+// Drop down menu with an item selected lost other options after fragment transition/rotation
+// https://github.com/material-components/material-components-android/issues/1464
+// not working
+class ExposedDropdownMenu(context: Context, attributeSet: AttributeSet?) :
+    MaterialAutoCompleteTextView(context, attributeSet) {
+
+    override fun getFreezesText(): Boolean {
+        return false
+    }
+}
+
+
--- app/src/main/java/com/example/readtrack/types/Status.kt
+++ app/src/main/java/com/example/readtrack/types/Status.kt
@@ -1,15 +1,21 @@
 package com.example.readtrack.types
 
-data class Status(val state: String) {
-    override fun toString(): String = state
+//data class Status(val state: String) {
+//    override fun toString(): String = state
+//
+//    companion object {
+//        @JvmStatic // To create a static get method for this property
+//                   // otherwise direct referencing: Status.states
+//        val states = listOf(
+//            Status("Read"),
+//            Status("Reading"),
+//            Status("Want to read")
+//        )
+//    }
+//}
 
-    companion object {
-        @JvmStatic // To create a static get method for this property
-                   // otherwise direct referencing: Status.states
-        val states = listOf(
-            Status("Read"),
-            Status("Reading"),
-            Status("Want to read")
-        )
-    }
+enum class Status(val status: String) {
+    READ("Read"),
+    READING("Reading"),
+    WANT_TO_READ("Want to read")
 }
